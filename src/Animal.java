f52bf48203db27615ff243849c985199d31ed1a3
Azamat Ray <250221@astanait.edu.kz>
2025-12-12 23:28:46 +0500

    test

---
+++ src/Animal.java
@@ -0,0 +1,13 @@
+public class Animal {
+    public static String iamDog() {
+        return "I am Dog";
+    }
+    public static void doStuff() {
+        int a = 5;
+        while(a < 50) {
+            System.out.println("hi");
+            a++;
+        }
+    }
+
+}
